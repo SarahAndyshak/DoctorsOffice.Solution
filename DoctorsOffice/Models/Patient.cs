6cf95d019332e8cb0c85d50587ec83c1e85dabfe
Brishna Bakshev <b.bakshev@gmail.com>
2023-03-13 16:26:26 -0700

    collabs
    Co-authored-by: Owen Brown <socknot@live.com>
    Co-authored-by: Jackson Levine <jack@netbug.us>
    Co-authored-by: Sarah Andyshak <sarah.andyshak@gmail.com>

--- DoctorsOffice/Models/Patient.cs
+++ DoctorsOffice/Models/Patient.cs
@@ -6,8 +6,9 @@ namespace DoctorsOffice.Models
   {
     public int PatientId { get; set; }
     public string Name { get; set; }
-    public int DoctorId { get; set; }
-    public Doctor Doctor { get; set; }
+    // public int DoctorId { get; set; }
+    // public Doctor Doctor { get; set; }
+    // comment these out so that each patient can have multiple doctors, use the List<DoctorPatient> instead
     public List<DoctorPatient> JoinEntities { get; }
   }
 }
